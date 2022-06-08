1c225ba2b33a999186b8dbd252eb905993d53bc7
ArthursLesinsh <lesinsh@gmail.com>
2022-06-08 19:32:50 +0300

    added x on click in the cell and asigned style to it

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const board = document.querySelector('.board');
-    
+
 for (let i = 1; i <= 9; i++) {
     let cell = document.createElement('a');
     cell.setAttribute('href', '#');
+    cell.onclick = function() {
+        this.textContent = 'x';
+    };
     board.append(cell);
 }
