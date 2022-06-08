3afecd92a6d963293b0e88b9c747364324d7a717
ArthursLesinsh <lesinsh@gmail.com>
2022-06-08 20:38:26 +0300

    Added function, where you can't add O are X if in cell there's some symbol.

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ for (let i = 1; i <= 9; i++) {
     let cell = document.createElement('a');
     cell.setAttribute('href', '#');
     cell.onclick = function() {
+        if (this.textContent != '') {
+            return;
+        }
         moves_count++; 
         if(moves_count % 2 == 0) {
             this.textContent = 'o';  
